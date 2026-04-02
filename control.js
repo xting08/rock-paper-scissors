4cbc019c803f6a6a0136312472f27c8c3bbe5c8c
xting08 <yoonxiaoting@gmail.com>
2026-04-02 22:02:19 +0800

    added some GUI

--- control.js
+++ control.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Please enter your choice: rock(r), paper(p), or scissors(s) \n You: " + humanScore + " | Computer: " + computerScore);
+    let choice = "Please enter your choice: rock(r), paper(p), or scissors(s) \n You: " + humanScore + " | Computer: " + computerScore;
     if (choice === "r" || choice === "R") {
         return "rock"; 
     } else if (choice === "p" || choice === "P") {
@@ -18,7 +18,7 @@ function getHumanChoice() {
     } else if (choice === "s" || choice === "S") {
         return "scissors";
     } else {
-        alert("Invalid choice");
+        console.log("Invalid choice");
         return getHumanChoice();
     }
 }
@@ -29,16 +29,18 @@ let round = 5;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        alert("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice + "\nIt's a tie!");
+        console.log("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice + "\nIt's a tie!");
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        alert("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
+        console.log("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
         humanScore += 1;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        alert("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
+        console.log("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
         humanScore += 1;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        alert("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
+        console.log("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou win!");
         humanScore += 1;
+    } else if (humanChoice === "quit") {
+        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nYou quit the game!");
     } else {
         alert("Human Choice: " + humanChoice + " | Computer Choice: " + computerChoice +"\nYou lose!");
         computerScore += 1;
@@ -52,13 +54,13 @@ function playGame() {
         playRound(getHumanChoice(), getComputerChoice());
     }
     if (humanScore > computerScore) {
-        alert("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nYou win the game!");
+        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nYou win the game!");
         playGame();
     } else if (humanScore < computerScore) {
-        alert("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nYou lose the game!");
+        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nYou lose the game!");
         playGame();
     } else {
-        alert("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nIt's a tie!");
+        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore + "\nIt's a tie!");
         playGame();
     }
 }
